171ed8ac360af87b2d71ee18b3b4544f4f32bf90
Gaurav <gauravkr2506@gmail.com>
2020-09-25 01:31:13 +0530

    Updates

---
+++ precache-manifest.dd26874107ffadbdee26cc393709c1d0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d64acc8bfef2bcf2d868a3b34b7babe9",
+    "url": "/fb-post-demo/index.html"
+  },
+  {
+    "revision": "061f43c279da0c71d3fa",
+    "url": "/fb-post-demo/static/css/main.7abd757a.chunk.css"
+  },
+  {
+    "revision": "83cedd979ee215a989ed",
+    "url": "/fb-post-demo/static/js/2.6e23e59d.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/fb-post-demo/static/js/2.6e23e59d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "061f43c279da0c71d3fa",
+    "url": "/fb-post-demo/static/js/main.a0e10314.chunk.js"
+  },
+  {
+    "revision": "98443db1e4ee94cb9166",
+    "url": "/fb-post-demo/static/js/runtime-main.03df7e58.js"
+  },
+  {
+    "revision": "a86c9563f7f17b70efdd9f00560263db",
+    "url": "/fb-post-demo/static/media/cross.a86c9563.svg"
+  },
+  {
+    "revision": "b0cae7486c0b30e1443f063dd8a0a1f6",
+    "url": "/fb-post-demo/static/media/discovery.b0cae748.png"
+  },
+  {
+    "revision": "d58c3c5aab229aa5c802a155e337008e",
+    "url": "/fb-post-demo/static/media/post.d58c3c5a.jpg"
+  }
+]);
